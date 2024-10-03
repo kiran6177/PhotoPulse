4badcd581da0ac73119c818c8774e6694388c332
kiran6177 <147698281+kiran6177@users.noreply.github.com>
2024-10-03 10:08:17 +0530

    feat:responsive-3

--- frontend/src/components/common/BorderLink.jsx
+++ frontend/src/components/common/BorderLink.jsx
@@ -6,7 +6,7 @@ function BorderLink({ title, styles, href }) {
     <div className="bg-gradient-to-r from-[#c14ac3] via-[#5010a2] to-[#2003b0] p-[2px] inline-block rounded-full">
       <div className="bg-black h-full rounded-full flex items-center">
         <Link
-          className=" text-white  px-4 py-[6px] tracking-widest text-xs"
+          className=" text-white  px-4 py-[6px] tracking-widest text-[10px] sm:text-xs"
           href={href}
         >
           {title}
